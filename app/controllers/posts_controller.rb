549b5f10eb8126062576ee17cf7752ca2faf541a
MR.Y <programming@yatsukouji.com>
2015-07-05 23:00:45 +0900

    before_actionの実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
 
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
   def new
     @post = Post.new
   end
@@ -11,7 +13,7 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
+    # @post = Post.find(params[:id])
   end
 
   def index
@@ -19,17 +21,17 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
+    # @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
+    # @post = Post.find(params[:id])
     @post.update_attributes(post_params)
     redirect_to "/posts/#{@post.id}"
   end
 
   def destroy
-    @post = Post.find(params[:id])
+    # @post = Post.find(params[:id])
     @post.destroy
     redirect_to "/"
   end
@@ -43,6 +45,10 @@ class PostsController < ApplicationController
       :body,
       :category
       )
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 
 end
