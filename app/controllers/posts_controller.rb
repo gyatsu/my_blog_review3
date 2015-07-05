3357ae979a519fddd025ed7f58749a2346713cdb
MR.Y <programming@yatsukouji.com>
2015-07-05 22:46:16 +0900

    edit実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,6 +18,16 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update_attributes(post_params)
+    redirect_to "/posts/#{@post.id}"
+  end
+
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
 
   get '/posts/' => 'posts#index'
 
+  get '/posts/:id/edit' => 'posts#edit'
+  patch '/posts/:id' => 'posts#update'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
