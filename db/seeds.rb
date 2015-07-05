bb39516d004966a1dfb659d914d3427003a2fdc7
MR.Y <programming@yatsukouji.com>
2015-07-05 23:50:45 +0900

    Categoryモデル追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Category.create(title: 'Rails')
+Category.create(title: 'Ruby')
+Category.create(title: 'etc')
